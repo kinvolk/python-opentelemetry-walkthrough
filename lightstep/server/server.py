330c9b8b4f4950bcd9a7d13631bc307c6f052e1c
Diego Hurtado <diegohurtado@gmail.com>
2019-10-02 10:44:43 -0600

    Investigating why span parent contexts are not being passed down

--- lightstep/server/server.py
+++ lightstep/server/server.py
@@ -7,10 +7,32 @@ from kitchen import KitchenService
 from status import NEW_ORDER, RECEIVED, COOKING, READY
 from donut import Donut
 
-from ipdb import set_trace
+import opentelemetry.ext.http_requests
+from opentelemetry import trace, propagators
+from opentelemetry.sdk.trace import Tracer
+from opentelemetry.ext.wsgi import OpenTelemetryMiddleware
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+from opentelemetry.sdk.trace.export import SimpleExportSpanProcessor
+from opentelemetry.sdk.context.propagation.b3_format import B3Format
+
+
+# Set the factory to be used to create the tracer
+trace.set_preferred_tracer_implementation(lambda T: Tracer())
+
+propagators.set_global_httptextformat(B3Format())
+
+opentelemetry.ext.http_requests.enable(trace.tracer())
+
+tracer = trace.tracer()
+
+tracer.add_span_processor(
+    SimpleExportSpanProcessor(ConsoleSpanExporter())
+)
 
 app = Flask(__name__)
 app.static_folder = 'static'
+
+app.wsgi_app = OpenTelemetryMiddleware(app.wsgi_app)
 
 
 kitchen_service = KitchenService()
@@ -25,27 +47,42 @@ def home():
 @app.route('/order', methods=['POST'])
 def order():
 
-    set_trace
-
     order_id = str(uuid4())
 
-    for donut_data in loads(next(request.form.keys()))['donuts']:
+    with tracer.start_span('root_span') as root_span:
 
-        for _ in range(donut_data['quantity']):
+        root_span
 
-            kitchen_service.add_donut(
-                Donut(donut_data['flavor'], order_id)
-            )
+        from ipdb import set_trace
+        set_trace()
 
-    return check_status(order_id)
+        for donut_data in loads(next(request.form.keys()))['donuts']:
+
+            for _ in range(donut_data['quantity']):
+
+                kitchen_service.add_donut(
+                    Donut(donut_data['flavor'], order_id)
+                )
+
+        return check_status(order_id)
 
 
 @app.route('/status', methods=['POST'])
 def status():
 
-    set_trace
+    # The issue is that tracer.get_current_span().parent is not root_span
 
-    return check_status(loads(next(request.form.keys()))['order_id'])
+    from ipdb import set_trace
+    set_trace()
+
+    with tracer.start_span('status_span') as status_span:
+
+        status_span
+
+        from ipdb import set_trace
+        set_trace()
+
+        return check_status(loads(next(request.form.keys()))['order_id'])
 
 
 def check_status(order_id):
